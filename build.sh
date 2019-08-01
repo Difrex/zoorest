2b5273c5242b0795955a1ea1926d14d1af7139a8
Denis Zheleztsov <difrex.punk@gmail.com>
2019-08-01 11:00:44 +0300

    [#2]: Force data marshaling into JSON object
    New path: */v1/get/path/to/the/node+json*
    Method: *GET*
    
    Fixes #2

--- build.sh
+++ build.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-cat > Dockerfile.builder <<EOF
-FROM golang
-
-MAINTAINER Denis Zheleztsov <difrex.punk@gmail.com>
-
-ENV GOPATH /usr
-
-RUN go get github.com/Difrex/zoorest/rest
-RUN cd /usr/src/github.com/Difrex/zoorest && go get -t -v ./...
-
-WORKDIR /usr/src/github.com/Difrex/zoorest
-
-ENTRYPOINT go build -ldflags "-linkmode external -extldflags -static" && mv zoorest /out
-EOF
-
-# Build builder
-docker build --no-cache -t zoorest_builder -f Dockerfile.builder .
-# Build bin
-docker run -v $(pwd)/out:/out zoorest_builder
-
-case $1 in alpine)
+case $1 in docker)
                docker build -t zoorest -f Dockerfile .
                ;;
+            binary)
+                go build -o zoorest .
+                ;;
+            docker-binary)
+                docker build --rm -t zoorest -f Dockerfile .
+                docker run --rm -u root -v $(pwd)/out:/tmp/out --entrypoint '/bin/cp' zoorest /bin/zoorest /tmp/out
+                ;;
             *)
-               ;;
+                ;;
 esac
-
